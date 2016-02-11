efa14823c1850179ee86556c669a3ac84751b363
Christian Schuermann <christian.schuermann@bluewin.ch>
2016-02-11 13:16:23 +0100

    Optimize plugin name

--- cmsplugin_plain_text/cms_plugins.py
+++ cmsplugin_plain_text/cms_plugins.py
@@ -1,11 +1,13 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext as _
 
 from .models import Plaintext
 
 
 class PlaintextPlugin(CMSPluginBase):
     model = Plaintext
+    name = _(u"Plaintext")
     render_template = 'cms/plugins/plaintext.html'
 
     def render(self, context, instance, placeholder):

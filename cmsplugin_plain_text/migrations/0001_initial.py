56dc94ad69fd1155cf6d7bc3af59ad23a3d99430
eXeCUT notebook <sexecut@gmail.com>
2020-02-13 22:44:01 +0300

    fixed tests for python 3

--- cmsplugin_plain_text/migrations/0001_initial.py
+++ cmsplugin_plain_text/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Plaintext',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=False)),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.CASCADE)),
                 ('body', models.TextField(verbose_name='Plaintext')),
             ],
             options={
--- tests/test_cms_plugins.py
+++ tests/test_cms_plugins.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import warnings
+
+from django.conf import settings
 from cms.api import add_plugin
 from cms.models import Placeholder
-from django.conf import settings
+from cms.plugin_rendering import ContentRenderer
 from django.test import TestCase
+from django.test.client import RequestFactory
 
 from cmsplugin_plain_text.cms_plugins import PlaintextPlugin
 
@@ -35,5 +40,6 @@ class CmsPluginPlainTextTestCase(TestCase):
             self.language,
             body=self.plaintext_body,
         )
-        html = model_instance.render_plugin({})
+        renderer = ContentRenderer(request=RequestFactory())
+        html = renderer.render_plugin(model_instance, {})
         self.assertEqual(html, self.plaintext_body)
